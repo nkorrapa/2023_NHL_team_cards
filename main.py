fec2b6bf2384370ca6dc48ad802be8860cb30208
Neha Korrapati <neha.korrapati@gmail.com>
2024-05-01 22:41:02 -0400

    updating structure

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 import time
 import pandas as pd
 import plotly.graph_objects as go
-
+import plotly.express as px
+# use container width = true
 ## functions
+st.set_page_config(page_title="2023 Team Scorecard", layout="wide")
 @st.cache_data()
 def load_file(file):
     time.sleep(3)
@@ -56,7 +58,7 @@ team_roster = get_roster(team_pbp)
 
 team_goals_labels = team_roster['Player']
 team_goals_values = team_roster['Goals']
-team_goals= go.Figure(data=[go.Pie(labels=team_goals_labels, values = team_goals_values, hole = 0.3)])
+team_goals= px.bar(team_roster, x='Player', y='Goals')
 
 
 
@@ -84,19 +86,53 @@ with st.container():
         st.write("Division: ", str(team_teams['division_rank'].values[0]))
 
 st.divider()
-st.subheader("Roster Points")
 with st.container():
-    st.dataframe(team_roster, hide_index=True)
-    st.plotly_chart(team_goals)
+    col1, col2 = st.columns(2)
+    with col1:
+      st.subheader("Roster Points")
+    with st.container():
+       st.dataframe(team_roster, hide_index=True, width=400)
+    with col2:
+        st.write("Team shooting % (INSERT PIE CHART HERE)") # TODO: add pie chart
 st.divider()
 
-st.subheader("Scoring")
+st.subheader("Top 10 Overall Scoring")
 with st.container():
+    tab1, tab2 = st.tabs(["Goals", "Assists"])
+    with tab1:
+      st.plotly_chart(team_goals, use_container_width=True) # TODO: Add Title, sort bar chart
+    with tab2: 
+        st.write("Insert Assists bar chart here")
+    st.divider()
+    st.subheader("Scoring Flow")
+    st.write("Add Sankey here") # TODO: Add sankey chart
+st.divider()
+
+st.subheader("Scoring - by Strength State")
+with st.container(): 
     tab1, tab2, tab3 = st.tabs(["Even Strength", "Power Play", "Shorthanded"])
     with tab1:
-        st.text("top 5 goals")
+      with st.container():
+        col1, col2 = st.columns(2)
+        with col1: 
+          st.text("goals - EV")
+        with col2:
+          st.text("assists - EV")      
+      st.write("Add Sankey here")
     with tab2:
-        st.text("top 5 goals")
+      with st.container(): #TODO: change to cols
+        col1, col2 = st.columns(2)
+        with col1: 
+          st.text("goals - PP")
+        with col2:
+          st.text("assists - PP")
+      st.write("Add Sankey here")        
     with tab3:
-        st.text("top 5 goals")
+      with st.container(): 
+        col1, col2 = st.columns(2)
+        with col1: 
+          st.text("goals - PK")
+        with col2:
+          st.text("assists - PK")  
+      st.write("Add sankey here")     
 
