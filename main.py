28bd8e2361fa2a5eb141468e00a1141f901d8e0e
Neha Korrapati <neha.korrapati@gmail.com>
2024-05-03 19:28:17 -0400

    added bar charts

--- main.py
+++ main.py
@@ -46,21 +46,70 @@ pbp = load_file("data/2023pbpfull.csv")
 st.title("2023 NHL Team Cards")
 
 team = st.sidebar.selectbox("Pick a NHL Team", teams['event_team'].tolist())
-#team = str(team)
+
 team_teams = teams[teams['event_team']== team]
 team_pbp = pbp[pbp.event_team == team]
 arena = "Home Arena: " + team_teams['home_arena'].values[0]
 
 record = str(team_teams['W'].values[0]) + "-" + str(team_teams['L'].values[0]) + "-" + str(team_teams['OT'].values[0])
 
+team_pbp_ev = pbp[(pbp['event_team'] == team) & (pbp['strength_code'] == 'EV')]
+team_pbp_pp = pbp[(pbp['event_team'] == team) & (pbp['strength_code'] == 'PP')]
+team_pbp_pk = pbp[(pbp['event_team'] == team) & (pbp['strength_code'] == 'SH')]
 ### DATA MANIP
+## ALL
 team_roster = get_roster(team_pbp)
 
-team_goals_labels = team_roster['Player']
-team_goals_values = team_roster['Goals']
-team_goals= px.bar(team_roster, x='Player', y='Goals')
+goals_10 = team_roster.sort_values(by = ['Goals'], ascending=False).head(10)
 
+goals_10_bar= px.bar(goals_10, x='Player', y='Goals', title= "Goals")
 
+assists_10 = team_roster.sort_values(by = ['Assists'], ascending=False).head(10)
+
+assists_10_bar = px.bar(assists_10, x = 'Player', y = 'Assists', title = "Assists")
+
+# sankey
+
+## EV
+team_roster_ev = get_roster(team_pbp_ev)
+
+goals_10_ev = team_roster_ev.sort_values(by = ['Goals'], ascending=False).head(10)
+
+goals_10_ev_bar= px.bar(goals_10_ev, x='Player', y='Goals', title= "Goals")
+
+assists_10_ev = team_roster_ev.sort_values(by = ['Assists'], ascending=False).head(10)
+
+assists_10_ev_bar = px.bar(assists_10_ev, x = 'Player', y = 'Assists', title = "Assists")
+
+# Sankey
+
+## PP
+
+team_roster_pp = get_roster(team_pbp_pp)
+
+goals_10_pp = team_roster_pp.sort_values(by = ['Goals'], ascending=False).head(10)
+
+goals_10_pp_bar= px.bar(goals_10_pp, x='Player', y='Goals', title= "Goals")
+
+assists_10_pp = team_roster_pp.sort_values(by = ['Assists'], ascending=False).head(10)
+
+assists_10_pp_bar = px.bar(assists_10_pp, x = 'Player', y = 'Assists', title = "Assists")
+
+# Sankey
+
+## PK
+
+team_roster_pk = get_roster(team_pbp_pk)
+
+goals_10_pk = team_roster_pk.sort_values(by = ['Goals'], ascending=False).head(10)
+
+goals_10_pk_bar= px.bar(goals_10_pk, x='Player', y='Goals', title= "Goals")
+
+assists_10_pk = team_roster_pk.sort_values(by = ['Assists'], ascending=False).head(10)
+
+assists_10_pk_bar = px.bar(assists_10_pk, x = 'Player', y = 'Assists', title = "Assists")
+
+# SANKEY
 
 
 ### BODY
@@ -100,9 +149,9 @@ st.subheader("Top 10 Overall Scoring")
 with st.container():
     tab1, tab2 = st.tabs(["Goals", "Assists"])
     with tab1:
-      st.plotly_chart(team_goals, use_container_width=True) # TODO: Add Title, sort bar chart
+      st.plotly_chart(goals_10_bar, use_container_width=True)
     with tab2: 
-        st.write("Insert Assists bar chart here")
+        st.plotly_chart(assists_10_bar, use_container_width=True)
     st.divider()
     st.subheader("Scoring Flow")
     st.write("Add Sankey here") # TODO: Add sankey chart
@@ -115,24 +164,24 @@ with st.container():
       with st.container():
         col1, col2 = st.columns(2)
         with col1: 
-          st.text("goals - EV")
+          st.plotly_chart(goals_10_ev_bar, use_container_width=True)
         with col2:
-          st.text("assists - EV")      
+          st.plotly_chart(assists_10_ev_bar, use_container_width=True)      
       st.write("Add Sankey here")
     with tab2:
-      with st.container(): #TODO: change to cols
+      with st.container():
         col1, col2 = st.columns(2)
         with col1: 
-          st.text("goals - PP")
+          st.plotly_chart(goals_10_pp_bar, use_container_width=True)
         with col2:
-          st.text("assists - PP")
+          st.plotly_chart(assists_10_pp_bar, use_container_width=True)
       st.write("Add Sankey here")        
     with tab3:
       with st.container(): 
         col1, col2 = st.columns(2)
         with col1: 
-          st.text("goals - PK")
+          st.plotly_chart(goals_10_pk_bar, use_container_width=True)
         with col2:
-          st.text("assists - PK")  
+          st.plotly_chart(assists_10_pk_bar, use_container_width=True)  
       st.write("Add sankey here")     
 
